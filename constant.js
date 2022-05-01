165e135b8c080c7e89b883a7ff3a7f8390c68e36
OleanjiKingCode <holamilean56@gmail.com>
2022-05-02 00:22:36 +0100

    using vrf

--- constant.js
+++ constant.js
@@ -1,8 +1,8 @@
 
-  export const LinkTokenAddress ="0x5c047072963E2c770D890f5e0ea10a1E3Ad6B9d9"
+  export const LinkTokenAddress ="0xeCb54640B3F9D899B4B1Ed9cA36421DdD44D2b03"
   export const OwnersAddress = "0xf2445f8FEEfef350ac1756F67C62938a37eDa375"
-  export const abiToken = ""
 
-  export const VotingAddress ="0x52C5fa57512d651180E3E31d724A68Cd73142C66"
-  export const abiVoting = ""
+
+  export const VotingAddress ="0x12Fe0078f4B5800e5f8CE524146a342197e89A6a"
+
   
